4bd1b52f848543f2d15734b9dcbcc0fd6c3c000b
Hobey Padlan <hobeypadlanxx@gmail.com>
2024-02-28 18:16:12 +0800

    fixed imports

--- app/src/main/java/com/example/insight/ui/StartScreen.kt
+++ app/src/main/java/com/example/insight/ui/StartScreen.kt
@@ -6,10 +6,6 @@ import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
